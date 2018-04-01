330e24ac11a35dc2f8091598b77866aaf7559353
Nicolas Angelov <nikovolt220@gmail.com>
2018-04-01 19:55:02 +0300

    Update vk_darkmode_css.meta.js

--- vk_darkmode_css.meta.js
+++ vk_darkmode_css.meta.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @run-at       document-start
 // @name         NightMode for VK
-// @version      1.0.1.2
+// @version      1.1.0.2
 // @description  Adds a nice, black-whity night mode for vk.
 // @author       https://vk.com/id71110013
 // @homepage     https://github.com/NIK220V/vk-darkmode-css
 // @updateURL    https://github.com/NIK220V/vk-darkmode-css/raw/master/vk_darkmode_css.meta.js
 // @downloadURL  https://github.com/NIK220V/vk-darkmode-css/raw/master/vk_darkmode_css.user.js
 // @match        *://*.vk.com/*
-// @grant        none
+// @grant        GM_getValue
+// @grant        GM_setValue
 // ==/UserScript==
